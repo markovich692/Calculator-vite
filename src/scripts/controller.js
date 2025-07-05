e2a28f952d6b08a2075135d8f48dfb34108c8f84
Marc Diallo <mark.devguru@outlook.com>
2025-07-05 17:40:52 -0500

    updates the screen on click of the digits buttons

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,11 +1,22 @@
 import "../sass/style.scss";
 
 const calcKeyboardButtons = document.querySelector(".keyboard__buttons");
+const screenDisplay = document.querySelector(".screen__display-text");
 
+calcKeyboardButtons.addEventListener("click", function (e) {
+  const btn = e.target;
 
+  if (!btn) return;
 
-// calcKeyboardButtons.forEach(function (btn, i, arr) {
-//   btn.addEventListener("click", function (e) {
-//     console.log(e.currentTarget);
-//   });
-// });
+  //DIGITS
+  if (btn.className.includes("keyboard__btn--digit")) {
+    const digitclicked = btn.textContent;
+
+    screenDisplay.textContent = digitclicked;
+  }
+
+  //DIGITS
+  if (btn.className.includes("keyboard__btn--fn")) {
+    console.log(btn.textContent);
+  }
+});
