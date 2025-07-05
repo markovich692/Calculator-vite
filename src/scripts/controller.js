23b5c0a39bb61949c3588db177c80c043bebcb1e
Marc Diallo <mark.devguru@outlook.com>
2025-07-05 17:21:40 -0500

    selects the entire block containing all of the buttons

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,5 +1,11 @@
 import "../sass/style.scss";
 
-const calcKeyboardButtons = Array.from(document.querySelectorAll(".keyboard__btn"));
+const calcKeyboardButtons = document.querySelector(".keyboard__buttons");
 
 
+
+// calcKeyboardButtons.forEach(function (btn, i, arr) {
+//   btn.addEventListener("click", function (e) {
+//     console.log(e.currentTarget);
+//   });
+// });
