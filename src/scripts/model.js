47e6b95014ed526e698fd2e6aec68478e0d16891
Marc Diallo <mark.devguru@outlook.com>
2025-07-16 09:36:42 -0500

    prevent invalid starting operators

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -9,8 +9,8 @@ export const state = {
   hasResult: false,
 };
 
-const operatorsAll = ["%", "/", "*", "-", "+", "="];
-const notStartWithOperator = ["%", "/", "x", "="];
+const operatorsAll = ["%", "/", "x", "-", "+", "="];
+const invalidStartOperator = ["%", "/", "x", "="];
 
 //DIGITS
 export const handleDigits = function (digit) {
@@ -30,65 +30,14 @@ export const handleDigits = function (digit) {
   }
 
   state.screenDisplay = state.currentInput;
+  console.log(state);
 };
 
-//OPERATORS
+//OPERATOR
 export const handleOperator = function (operator) {
-  //Prevents the currentInput to overflow the screen
-  if (state.currentInput.length > 12) return;
-
-  //Prevent starting with operators "%", "/", "*", "="
-  if (!state.currentInput && notStartWithOperator.includes(operator)) return;
-
-  //Prevent 2 consecutive operators
-  if (
-    operatorsAll.includes(state.currentInput[state.currentInput.length - 1]) &&
-    operatorsAll.includes(operator)
-  )
+  //Prevent invalid starts operator
+  if (state.currentInput === "" && invalidStartOperator.includes(operator))
     return;
 
-  //Adds currenInput to the toDisplayArray an resets it
-  if (state.currentInput !== "") {
-    state.toDisplayArray.push(state.currentInput);
-    state.currentInput = "";
-  }
-
-
-
-  //Updates the screenDisplay property
-  state.screenDisplay = state.currentInput;
+  
 };
-
-/*
-export const updateDisplayFull = function (btnClicked) {
-  const operatorsAll = ["-", "+", "/", "%", "x", "=", "."];
-  const operatorsStart = ["/", "%", "x", "=", "."];
-  const lastElementDisplay = state.displayFull[state.displayFull.length - 1];
-
-  //Make sure that equal can only be used when the state.displayFull.length is at least 3
-  if (state.displayFull.length < 3 && btnClicked === "=") return;
-
-  //Prevent the calculation to start with a zero
-  if (!state.displayFull.length && btnClicked === "0") return;
-
-  //Prevent the calculation to start with any operator
-  if (!state.displayFull.length && operatorsStart.includes(btnClicked)) return;
-
-  //Prevent the 2 operators from following each other
-  if (
-    operatorsAll.includes(lastElementDisplay) &&
-    operatorsAll.includes(btnClicked)
-  )
-    return;
-
-  state.currentInput = "";
-
-  state.displayFull.push(btnClicked);
-
-  const rendered = state.displayFull.join("");
-
-  state.screenDisplay = rendered;
-};
-
-export const evaluateExpression = function () {};
-*/
